43c0f642389818277ad58a9f02ec2890f7b75501
Leon Boot <leon@nedbase.nl>
2022-08-11 16:35:26 +0200

    Removed sensio/framework-extra-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,6 @@ return [
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Http\HttplugBundle\HttplugBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
